b5fb4020c74dab402ed2365a3c0cd94a53425c91
Marek Knápek <knapek.mar@gmail.com>
2022-03-19 06:16:55 +0100

    reorganize

---
+++ libs/mk_int.c
@@ -0,0 +1,14 @@
+#include "../../mk_int/src/base/mk_uint_base_char.c"
+#include "../../mk_int/src/base/mk_uint_base_short.c"
+#include "../../mk_int/src/base/mk_uint_base_int.c"
+#include "../../mk_int/src/base/mk_uint_base_long.c"
+#include "../../mk_int/src/base/mk_uint_base_llong.c"
+#include "../../mk_int/src/base/mk_uint_base_128.c"
+
+#include "../../mk_int/src/exact/mk_uint_8.c"
+#include "../../mk_int/src/exact/mk_uint_16.c"
+#include "../../mk_int/src/exact/mk_uint_32.c"
+#include "../../mk_int/src/exact/mk_uint_64.c"
+#include "../../mk_int/src/exact/mk_uint_128.c"
+
+#include "../../mk_int/src/utils/maybe_initialize.c"
