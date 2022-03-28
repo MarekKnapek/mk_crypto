4c1dafb325bb5fda39210a28b3661186631c2549
Marek Knápek <knapek.mar@gmail.com>
2022-03-28 02:06:39 +0200

    More complex SHA1 fuzzing.

--- fuzz/mk_sha1_fuzz.c
+++ fuzz/mk_sha1_fuzz.c
@@ -3,6 +3,8 @@
 #include "../src/mk_sha1.h"
 #include "../src/mk_win_sha1.h"
 
+#include "../utils/mk_inline.h"
+
 #include <stddef.h> /* size_t NULL */
 #include <string.h> /* memcmp */
 
@@ -10,7 +12,7 @@
 #define test(x) do{ if(!(x)){ int volatile* volatile ptr = NULL; *ptr = 0; } }while(0)
 
 
-void mk_sha1_fuzz(unsigned char const* data, size_t size)
+static mk_inline void mk_sha1_fuzz_basic(unsigned char const* data, size_t size)
 {
 	struct mk_win_sha1_state_s mk_win_sha1_state;
 	unsigned char mk_win_sha1_digest[20];
@@ -26,7 +28,64 @@ void mk_sha1_fuzz(unsigned char const* data, size_t size)
 	mk_sha1_append(&mk_sha1_state, data, size);
 	mk_sha1_finish(&mk_sha1_state, &mk_sha1_digest);
 
-	test(memcmp(&mk_sha1_digest, &mk_win_sha1_digest, 20) == 0);
+	test(memcmp(&mk_sha1_digest, &mk_win_sha1_digest, sizeof(mk_sha1_digest)) == 0);
+}
+
+static mk_inline void mk_sha1_fuzz_complex(unsigned char const* data, size_t size)
+{
+	struct mk_win_sha1_state_s mk_win_sha1_state;
+	unsigned char mk_win_sha1_digest[20];
+
+	struct mk_sha1_state_s mk_sha1_state;
+	unsigned char mk_sha1_digest[20];
+
+	unsigned parts;
+	unsigned i;
+	unsigned part_len;
+
+	if(size < 2)
+	{
+		return;
+	}
+	parts = ((unsigned)(data[0]) << CHAR_BIT) | ((unsigned)(data[1]));
+	data += 2;
+	size -= 2;
+	mk_win_sha1_init(&mk_win_sha1_state);
+	mk_sha1_init(&mk_sha1_state);
+	for(i = 0; i != parts; ++i)
+	{
+		if(size < 2)
+		{
+			mk_win_sha1_finish(&mk_win_sha1_state, &mk_win_sha1_digest);
+			mk_sha1_finish(&mk_sha1_state, &mk_sha1_digest);
+			test(memcmp(&mk_sha1_digest, &mk_win_sha1_digest, sizeof(mk_sha1_digest)) == 0);
+			return;
+		}
+		part_len = ((unsigned)(data[0]) << CHAR_BIT) | ((unsigned)(data[1]));
+		data += 2;
+		size -= 2;
+		if(size < part_len)
+		{
+			mk_win_sha1_finish(&mk_win_sha1_state, &mk_win_sha1_digest);
+			mk_sha1_finish(&mk_sha1_state, &mk_sha1_digest);
+			test(memcmp(&mk_sha1_digest, &mk_win_sha1_digest, sizeof(mk_sha1_digest)) == 0);
+			return;
+		}
+		mk_win_sha1_append(&mk_win_sha1_state, data, part_len);
+		mk_sha1_append(&mk_sha1_state, data, part_len);
+		data += part_len;
+		size -= part_len;
+	}
+	mk_win_sha1_finish(&mk_win_sha1_state, &mk_win_sha1_digest);
+	mk_sha1_finish(&mk_sha1_state, &mk_sha1_digest);
+	test(memcmp(&mk_sha1_digest, &mk_win_sha1_digest, sizeof(mk_sha1_digest)) == 0);
+}
+
+
+void mk_sha1_fuzz(unsigned char const* data, size_t size)
+{
+	mk_sha1_fuzz_basic(data, size);
+	mk_sha1_fuzz_complex(data, size);
 }
 
 
