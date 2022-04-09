b459425460237a8c2b1d999fa94fd289f9986544
Marek Knápek <knapek.mar@gmail.com>
2022-04-09 16:26:27 +0200

    Add tests.  Example values.

---
+++ projects/jumbo/tests/tests.c
@@ -0,0 +1,48 @@
+#if defined(_MSC_VER)
+#pragma warning(push)
+#pragma warning(disable:4710)/* warning C4710: 'xxx': function not inlined */
+#pragma warning(disable:4464) /* warning C4464: relative include path contains '..' */
+#endif
+
+
+#include "../../../tests/mk_sha3_test_examples.h"
+
+#include <stdio.h> /* printf */
+
+
+int main(void)
+{
+	int tested;
+
+	tested = mk_sha3_test_examples();
+	if(tested == 0)
+	{
+		printf("Success!\n");
+		return 0;
+	}
+	else
+	{
+		printf("Failure at line %d!\n", tested);
+		return tested;
+	}
+}
+
+
+#include "../../../tests/mk_sha3_test_examples.c"
+
+#include "../../../src/mk_sha3_224.c"
+#include "../../../src/mk_sha3_256.c"
+#include "../../../src/mk_sha3_384.c"
+#include "../../../src/mk_sha3_512.c"
+
+#include "../../../src/mk_sha3_detail.c"
+
+#include "../../../../mk_int/src/exact/mk_uint_64.c"
+
+#include "../../../../mk_int/src/base/mk_uint_base_long.c"
+#include "../../../../mk_int/src/base/mk_uint_base_llong.c"
+
+
+#if defined(_MSC_VER)
+#pragma warning(pop)
+#endif
