274009781dc7b87189b04109a292b34a5f0b4669
Marek Knápek <knapek.mar@gmail.com>
2022-05-05 03:50:58 +0200

    Move from stack to heap.
    MS-DOS has problems with it.

--- apps/mkcryptc/mkcryptc.c
+++ apps/mkcryptc/mkcryptc.c
@@ -7,7 +7,7 @@
 #include <limits.h> /* LONG_MAX LONG_MIN */
 #include <stddef.h> /* NULL */
 #include <stdio.h> /* FILE size_t fopen fread fclose */
-#include <stdlib.h> /* strtol srand rand */
+#include <stdlib.h> /* malloc free strtol srand rand */
 #include <string.h> /* strlen memcmp */
 #include <time.h> /* time time_t */
 
@@ -45,10 +45,7 @@ static char const s_pbkdf2_hash_hmac_sha3_384[] = "hmac-sha3-384";
 static char const s_pbkdf2_hash_hmac_sha3_512[] = "hmac-sha3-512";
 
 
-#define check(x) do{ if(!(x)){ return __LINE__; } }while(0)
-
-
-int mkcryptc(int argc_, char const* const* argv_)
+struct main_object_s
 {
 	int argc;
 	char const* const* argv;
@@ -78,199 +75,213 @@ int mkcryptc(int argc_, char const* const* argv_)
 	unsigned char* buffer_a;
 	unsigned char buffer_1[512 + mk_block_cipher_block_len_max];
 	unsigned char* buffer_b;
-	unsigned char buffer_2[sizeof(buffer_1)];
+	unsigned char buffer_2[512 + mk_block_cipher_block_len_max];
 	size_t read_b;
 	int crypted;
 	unsigned char* buffer_t;
 	size_t read_t;
 	int fclosed;
+};
 
-	argc = argc_ - 1;
-	argv = argv_ + 1;
 
-	check(argc >= 1);
-	len = (int)strlen(*argv);
-	if(len == sizeof(s_command_encrypt) - 1 && memcmp(*argv, s_command_encrypt, sizeof(s_command_encrypt) - 1) == 0){ direction = 0; }
-	else if(len == sizeof(s_command_decrypt) - 1 && memcmp(*argv, s_command_decrypt, sizeof(s_command_decrypt) - 1) == 0){ direction = 1; }
+#define check(x) do{ if(!(x)){ return __LINE__; } }while(0)
+
+
+int mkcryptc(int argc_, char const* const* argv_)
+{
+	struct main_object_s* mo;
+
+	mo = (struct main_object_s*)malloc(sizeof(struct main_object_s));
+	check(mo);
+
+	mo->argc = argc_ - 1;
+	mo->argv = argv_ + 1;
+
+	check(mo->argc >= 1);
+	mo->len = (int)strlen(*mo->argv);
+	if(mo->len == sizeof(s_command_encrypt) - 1 && memcmp(*mo->argv, s_command_encrypt, sizeof(s_command_encrypt) - 1) == 0){ mo->direction = 0; }
+	else if(mo->len == sizeof(s_command_decrypt) - 1 && memcmp(*mo->argv, s_command_decrypt, sizeof(s_command_decrypt) - 1) == 0){ mo->direction = 1; }
 	else{ check(0); }
-	--argc;
-	++argv;
+	--mo->argc;
+	++mo->argv;
 
-	cfb_f = -1;
-	ctr_be = -1;
-	check(argc >= 1);
-	len = (int)strlen(*argv);
-	if(len == sizeof(s_operation_mode_cbc) - 1 && memcmp(*argv, s_operation_mode_cbc, sizeof(s_operation_mode_cbc) - 1) == 0){ operation_mode = mk_operation_mode_cbc; }
-	else if(len == sizeof(s_operation_mode_cfb) - 1 && memcmp(*argv, s_operation_mode_cfb, sizeof(s_operation_mode_cfb) - 1) == 0){ operation_mode = mk_operation_mode_cfb; cfb_f = 0; }
-	else if(len == sizeof(s_operation_mode_cfb_f) - 1 && memcmp(*argv, s_operation_mode_cfb_f, sizeof(s_operation_mode_cfb_f) - 1) == 0){ operation_mode = mk_operation_mode_cfb; cfb_f = 1; }
-	else if(len == sizeof(s_operation_mode_ctr) - 1 && memcmp(*argv, s_operation_mode_ctr, sizeof(s_operation_mode_ctr) - 1) == 0){ operation_mode = mk_operation_mode_ctr; ctr_be = 0; }
-	else if(len == sizeof(s_operation_mode_ctr_be) - 1 && memcmp(*argv, s_operation_mode_ctr_be, sizeof(s_operation_mode_ctr_be) - 1) == 0){ operation_mode = mk_operation_mode_ctr; ctr_be = 1; }
-	else if(len == sizeof(s_operation_mode_ecb) - 1 && memcmp(*argv, s_operation_mode_ecb, sizeof(s_operation_mode_ecb) - 1) == 0){ operation_mode = mk_operation_mode_ecb; }
-	else if(len == sizeof(s_operation_mode_ofb) - 1 && memcmp(*argv, s_operation_mode_ofb, sizeof(s_operation_mode_ofb) - 1) == 0){ operation_mode = mk_operation_mode_ofb; }
+	mo->cfb_f = -1;
+	mo->ctr_be = -1;
+	check(mo->argc >= 1);
+	mo->len = (int)strlen(*mo->argv);
+	if(mo->len == sizeof(s_operation_mode_cbc) - 1 && memcmp(*mo->argv, s_operation_mode_cbc, sizeof(s_operation_mode_cbc) - 1) == 0){ mo->operation_mode = mk_operation_mode_cbc; }
+	else if(mo->len == sizeof(s_operation_mode_cfb) - 1 && memcmp(*mo->argv, s_operation_mode_cfb, sizeof(s_operation_mode_cfb) - 1) == 0){ mo->operation_mode = mk_operation_mode_cfb; mo->cfb_f = 0; }
+	else if(mo->len == sizeof(s_operation_mode_cfb_f) - 1 && memcmp(*mo->argv, s_operation_mode_cfb_f, sizeof(s_operation_mode_cfb_f) - 1) == 0){ mo->operation_mode = mk_operation_mode_cfb; mo->cfb_f = 1; }
+	else if(mo->len == sizeof(s_operation_mode_ctr) - 1 && memcmp(*mo->argv, s_operation_mode_ctr, sizeof(s_operation_mode_ctr) - 1) == 0){ mo->operation_mode = mk_operation_mode_ctr; mo->ctr_be = 0; }
+	else if(mo->len == sizeof(s_operation_mode_ctr_be) - 1 && memcmp(*mo->argv, s_operation_mode_ctr_be, sizeof(s_operation_mode_ctr_be) - 1) == 0){ mo->operation_mode = mk_operation_mode_ctr; mo->ctr_be = 1; }
+	else if(mo->len == sizeof(s_operation_mode_ecb) - 1 && memcmp(*mo->argv, s_operation_mode_ecb, sizeof(s_operation_mode_ecb) - 1) == 0){ mo->operation_mode = mk_operation_mode_ecb; }
+	else if(mo->len == sizeof(s_operation_mode_ofb) - 1 && memcmp(*mo->argv, s_operation_mode_ofb, sizeof(s_operation_mode_ofb) - 1) == 0){ mo->operation_mode = mk_operation_mode_ofb; }
 	else{ check(0); }
-	--argc;
-	++argv;
+	--mo->argc;
+	++mo->argv;
 
-	check(argc >= 1);
-	len = (int)strlen(*argv);
-	if(len == sizeof(s_block_cipher_aes128) - 1 && memcmp(*argv, s_block_cipher_aes128, sizeof(s_block_cipher_aes128) - 1) == 0){ block_cipher = mk_block_cipher_aes128; }
-	else if(len == sizeof(s_block_cipher_aes192) - 1 && memcmp(*argv, s_block_cipher_aes192, sizeof(s_block_cipher_aes192) - 1) == 0){ block_cipher = mk_block_cipher_aes192; }
-	else if(len == sizeof(s_block_cipher_aes256) - 1 && memcmp(*argv, s_block_cipher_aes256, sizeof(s_block_cipher_aes256) - 1) == 0){ block_cipher = mk_block_cipher_aes256; }
+	check(mo->argc >= 1);
+	mo->len = (int)strlen(*mo->argv);
+	if(mo->len == sizeof(s_block_cipher_aes128) - 1 && memcmp(*mo->argv, s_block_cipher_aes128, sizeof(s_block_cipher_aes128) - 1) == 0){ mo->block_cipher = mk_block_cipher_aes128; }
+	else if(mo->len == sizeof(s_block_cipher_aes192) - 1 && memcmp(*mo->argv, s_block_cipher_aes192, sizeof(s_block_cipher_aes192) - 1) == 0){ mo->block_cipher = mk_block_cipher_aes192; }
+	else if(mo->len == sizeof(s_block_cipher_aes256) - 1 && memcmp(*mo->argv, s_block_cipher_aes256, sizeof(s_block_cipher_aes256) - 1) == 0){ mo->block_cipher = mk_block_cipher_aes256; }
 	else{ check(0); }
-	--argc;
-	++argv;
+	--mo->argc;
+	++mo->argv;
 
-	check(argc >= 1);
-	len = (int)strlen(*argv);
-	if(len == sizeof(s_kdf_pbkdf2) - 1 && memcmp(*argv, s_kdf_pbkdf2, sizeof(s_kdf_pbkdf2) - 1) == 0){}
+	check(mo->argc >= 1);
+	mo->len = (int)strlen(*mo->argv);
+	if(mo->len == sizeof(s_kdf_pbkdf2) - 1 && memcmp(*mo->argv, s_kdf_pbkdf2, sizeof(s_kdf_pbkdf2) - 1) == 0){}
 	else{ check(0); }
-	--argc;
-	++argv;
+	--mo->argc;
+	++mo->argv;
 
-	check(argc >= 1);
-	len = (int)strlen(*argv);
-	if(len == sizeof(s_pbkdf2_hash_hmac_md2) - 1 && memcmp(*argv, s_pbkdf2_hash_hmac_md2, sizeof(s_pbkdf2_hash_hmac_md2) - 1) == 0){ hash_id = mk_pbkdf2_hash_md2; }
-	else if(len == sizeof(s_pbkdf2_hash_hmac_md4) - 1 && memcmp(*argv, s_pbkdf2_hash_hmac_md4, sizeof(s_pbkdf2_hash_hmac_md4) - 1) == 0){ hash_id = mk_pbkdf2_hash_md4; }
-	else if(len == sizeof(s_pbkdf2_hash_hmac_md5) - 1 && memcmp(*argv, s_pbkdf2_hash_hmac_md5, sizeof(s_pbkdf2_hash_hmac_md5) - 1) == 0){ hash_id = mk_pbkdf2_hash_md5; }
-	else if(len == sizeof(s_pbkdf2_hash_hmac_sha1) - 1 && memcmp(*argv, s_pbkdf2_hash_hmac_sha1, sizeof(s_pbkdf2_hash_hmac_sha1) - 1) == 0){ hash_id = mk_pbkdf2_hash_sha1; }
-	else if(len == sizeof(s_pbkdf2_hash_hmac_sha2_224) - 1 && memcmp(*argv, s_pbkdf2_hash_hmac_sha2_224, sizeof(s_pbkdf2_hash_hmac_sha2_224) - 1) == 0){ hash_id = mk_pbkdf2_hash_sha2_224; }
-	else if(len == sizeof(s_pbkdf2_hash_hmac_sha2_256) - 1 && memcmp(*argv, s_pbkdf2_hash_hmac_sha2_256, sizeof(s_pbkdf2_hash_hmac_sha2_256) - 1) == 0){ hash_id = mk_pbkdf2_hash_sha2_256; }
-	else if(len == sizeof(s_pbkdf2_hash_hmac_sha2_384) - 1 && memcmp(*argv, s_pbkdf2_hash_hmac_sha2_384, sizeof(s_pbkdf2_hash_hmac_sha2_384) - 1) == 0){ hash_id = mk_pbkdf2_hash_sha2_384; }
-	else if(len == sizeof(s_pbkdf2_hash_hmac_sha2_512) - 1 && memcmp(*argv, s_pbkdf2_hash_hmac_sha2_512, sizeof(s_pbkdf2_hash_hmac_sha2_512) - 1) == 0){ hash_id = mk_pbkdf2_hash_sha2_512; }
-	else if(len == sizeof(s_pbkdf2_hash_hmac_sha2_512224) - 1 && memcmp(*argv, s_pbkdf2_hash_hmac_sha2_512224, sizeof(s_pbkdf2_hash_hmac_sha2_512224) - 1) == 0){ hash_id = mk_pbkdf2_hash_sha2_512224; }
-	else if(len == sizeof(s_pbkdf2_hash_hmac_sha2_512256) - 1 && memcmp(*argv, s_pbkdf2_hash_hmac_sha2_512256, sizeof(s_pbkdf2_hash_hmac_sha2_512256) - 1) == 0){ hash_id = mk_pbkdf2_hash_sha2_512256; }
-	else if(len == sizeof(s_pbkdf2_hash_hmac_sha3_224) - 1 && memcmp(*argv, s_pbkdf2_hash_hmac_sha3_224, sizeof(s_pbkdf2_hash_hmac_sha3_224) - 1) == 0){ hash_id = mk_pbkdf2_hash_sha3_224; }
-	else if(len == sizeof(s_pbkdf2_hash_hmac_sha3_256) - 1 && memcmp(*argv, s_pbkdf2_hash_hmac_sha3_256, sizeof(s_pbkdf2_hash_hmac_sha3_256) - 1) == 0){ hash_id = mk_pbkdf2_hash_sha3_256; }
-	else if(len == sizeof(s_pbkdf2_hash_hmac_sha3_384) - 1 && memcmp(*argv, s_pbkdf2_hash_hmac_sha3_384, sizeof(s_pbkdf2_hash_hmac_sha3_384) - 1) == 0){ hash_id = mk_pbkdf2_hash_sha3_384; }
-	else if(len == sizeof(s_pbkdf2_hash_hmac_sha3_512) - 1 && memcmp(*argv, s_pbkdf2_hash_hmac_sha3_512, sizeof(s_pbkdf2_hash_hmac_sha3_512) - 1) == 0){ hash_id = mk_pbkdf2_hash_sha3_512; }
+	check(mo->argc >= 1);
+	mo->len = (int)strlen(*mo->argv);
+	if(mo->len == sizeof(s_pbkdf2_hash_hmac_md2) - 1 && memcmp(*mo->argv, s_pbkdf2_hash_hmac_md2, sizeof(s_pbkdf2_hash_hmac_md2) - 1) == 0){ mo->hash_id = mk_pbkdf2_hash_md2; }
+	else if(mo->len == sizeof(s_pbkdf2_hash_hmac_md4) - 1 && memcmp(*mo->argv, s_pbkdf2_hash_hmac_md4, sizeof(s_pbkdf2_hash_hmac_md4) - 1) == 0){ mo->hash_id = mk_pbkdf2_hash_md4; }
+	else if(mo->len == sizeof(s_pbkdf2_hash_hmac_md5) - 1 && memcmp(*mo->argv, s_pbkdf2_hash_hmac_md5, sizeof(s_pbkdf2_hash_hmac_md5) - 1) == 0){ mo->hash_id = mk_pbkdf2_hash_md5; }
+	else if(mo->len == sizeof(s_pbkdf2_hash_hmac_sha1) - 1 && memcmp(*mo->argv, s_pbkdf2_hash_hmac_sha1, sizeof(s_pbkdf2_hash_hmac_sha1) - 1) == 0){ mo->hash_id = mk_pbkdf2_hash_sha1; }
+	else if(mo->len == sizeof(s_pbkdf2_hash_hmac_sha2_224) - 1 && memcmp(*mo->argv, s_pbkdf2_hash_hmac_sha2_224, sizeof(s_pbkdf2_hash_hmac_sha2_224) - 1) == 0){ mo->hash_id = mk_pbkdf2_hash_sha2_224; }
+	else if(mo->len == sizeof(s_pbkdf2_hash_hmac_sha2_256) - 1 && memcmp(*mo->argv, s_pbkdf2_hash_hmac_sha2_256, sizeof(s_pbkdf2_hash_hmac_sha2_256) - 1) == 0){ mo->hash_id = mk_pbkdf2_hash_sha2_256; }
+	else if(mo->len == sizeof(s_pbkdf2_hash_hmac_sha2_384) - 1 && memcmp(*mo->argv, s_pbkdf2_hash_hmac_sha2_384, sizeof(s_pbkdf2_hash_hmac_sha2_384) - 1) == 0){ mo->hash_id = mk_pbkdf2_hash_sha2_384; }
+	else if(mo->len == sizeof(s_pbkdf2_hash_hmac_sha2_512) - 1 && memcmp(*mo->argv, s_pbkdf2_hash_hmac_sha2_512, sizeof(s_pbkdf2_hash_hmac_sha2_512) - 1) == 0){ mo->hash_id = mk_pbkdf2_hash_sha2_512; }
+	else if(mo->len == sizeof(s_pbkdf2_hash_hmac_sha2_512224) - 1 && memcmp(*mo->argv, s_pbkdf2_hash_hmac_sha2_512224, sizeof(s_pbkdf2_hash_hmac_sha2_512224) - 1) == 0){ mo->hash_id = mk_pbkdf2_hash_sha2_512224; }
+	else if(mo->len == sizeof(s_pbkdf2_hash_hmac_sha2_512256) - 1 && memcmp(*mo->argv, s_pbkdf2_hash_hmac_sha2_512256, sizeof(s_pbkdf2_hash_hmac_sha2_512256) - 1) == 0){ mo->hash_id = mk_pbkdf2_hash_sha2_512256; }
+	else if(mo->len == sizeof(s_pbkdf2_hash_hmac_sha3_224) - 1 && memcmp(*mo->argv, s_pbkdf2_hash_hmac_sha3_224, sizeof(s_pbkdf2_hash_hmac_sha3_224) - 1) == 0){ mo->hash_id = mk_pbkdf2_hash_sha3_224; }
+	else if(mo->len == sizeof(s_pbkdf2_hash_hmac_sha3_256) - 1 && memcmp(*mo->argv, s_pbkdf2_hash_hmac_sha3_256, sizeof(s_pbkdf2_hash_hmac_sha3_256) - 1) == 0){ mo->hash_id = mk_pbkdf2_hash_sha3_256; }
+	else if(mo->len == sizeof(s_pbkdf2_hash_hmac_sha3_384) - 1 && memcmp(*mo->argv, s_pbkdf2_hash_hmac_sha3_384, sizeof(s_pbkdf2_hash_hmac_sha3_384) - 1) == 0){ mo->hash_id = mk_pbkdf2_hash_sha3_384; }
+	else if(mo->len == sizeof(s_pbkdf2_hash_hmac_sha3_512) - 1 && memcmp(*mo->argv, s_pbkdf2_hash_hmac_sha3_512, sizeof(s_pbkdf2_hash_hmac_sha3_512) - 1) == 0){ mo->hash_id = mk_pbkdf2_hash_sha3_512; }
 	else{ check(0); }
-	--argc;
-	++argv;
+	--mo->argc;
+	++mo->argv;
 
-	check(argc >= 1);
-	password_len = (int)strlen(*argv);
-	password = *argv;
-	--argc;
-	++argv;
+	check(mo->argc >= 1);
+	mo->password_len = (int)strlen(*mo->argv);
+	mo->password = *mo->argv;
+	--mo->argc;
+	++mo->argv;
 
-	check(argc >= 1);
-	salt_len = (int)strlen(*argv);
-	salt = *argv;
-	--argc;
-	++argv;
+	check(mo->argc >= 1);
+	mo->salt_len = (int)strlen(*mo->argv);
+	mo->salt = *mo->argv;
+	--mo->argc;
+	++mo->argv;
 
-	check(argc >= 1);
-	len = (int)strlen(*argv);
-	iterations = strtol(*argv, &endp, 10);
-	check(endp != *argv && endp == *argv + len && iterations != 0 && iterations != LONG_MAX && iterations != LONG_MIN && iterations > 0);
-	--argc;
-	++argv;
+	check(mo->argc >= 1);
+	mo->len = (int)strlen(*mo->argv);
+	mo->iterations = strtol(*mo->argv, &mo->endp, 10);
+	check(mo->endp != *mo->argv && mo->endp == *mo->argv + mo->len && mo->iterations != 0 && mo->iterations != LONG_MAX && mo->iterations != LONG_MIN && mo->iterations > 0);
+	--mo->argc;
+	++mo->argv;
 
-	check(argc >= 1);
-	input_file_name = *argv;
-	--argc;
-	++argv;
+	check(mo->argc >= 1);
+	mo->input_file_name = *mo->argv;
+	--mo->argc;
+	++mo->argv;
 
-	check(argc >= 1);
-	output_file_name = *argv;
-	--argc;
-	++argv;
+	check(mo->argc >= 1);
+	mo->output_file_name = *mo->argv;
+	--mo->argc;
+	++mo->argv;
 
-	mk_pbkdf2(hash_id, password, password_len, salt, salt_len, iterations, mk_block_cipher_get_key_len(block_cipher), key);
+	mk_pbkdf2(mo->hash_id, mo->password, mo->password_len, mo->salt, mo->salt_len, mo->iterations, mk_block_cipher_get_key_len(mo->block_cipher), mo->key);
 
-	mk_crypt_init(&crypt, operation_mode, block_cipher, key);
-	if(operation_mode == mk_operation_mode_cfb && cfb_f == 1)
+	mk_crypt_init(&mo->crypt, mo->operation_mode, mo->block_cipher, mo->key);
+	if(mo->operation_mode == mk_operation_mode_cfb && mo->cfb_f == 1)
 	{
-		mk_crypt_set_param_cfb_s_bytes(&crypt, mk_block_cipher_get_block_len(block_cipher));
+		mk_crypt_set_param_cfb_s_bytes(&mo->crypt, mk_block_cipher_get_block_len(mo->block_cipher));
 	}
-	if(operation_mode == mk_operation_mode_ctr && ctr_be == 1)
+	if(mo->operation_mode == mk_operation_mode_ctr && mo->ctr_be == 1)
 	{
-		mk_crypt_set_param_ctr_endian(&crypt, 1);
+		mk_crypt_set_param_ctr_endian(&mo->crypt, 1);
 	}
 
-	input_file = fopen(input_file_name, "rb");
-	check(input_file);
+	mo->input_file = fopen(mo->input_file_name, "rb");
+	check(mo->input_file);
 
-	output_file = fopen(output_file_name, "wb");
-	check(output_file);
+	mo->output_file = fopen(mo->output_file_name, "wb");
+	check(mo->output_file);
 
-	if(direction == 0)
+	if(mo->direction == 0)
 	{
 		srand((unsigned)time(NULL));
-		for(i = 0; i != sizeof(iv); ++i)
+		for(mo->i = 0; mo->i != sizeof(mo->iv); ++mo->i)
 		{
-			iv[i] = rand() % 0xff;
+			mo->iv[mo->i] = rand() % 0xff;
 		}
 
-		written = fwrite(iv, 1, sizeof(iv), output_file);
-		check(written == sizeof(iv));
+		mo->written = fwrite(mo->iv, 1, sizeof(mo->iv), mo->output_file);
+		check(mo->written == sizeof(mo->iv));
 	}
 	else
 	{
-		read_a = fread(iv, 1, sizeof(iv), input_file);
-		check(read_a == sizeof(iv));
+		mo->read_a = fread(mo->iv, 1, sizeof(mo->iv), mo->input_file);
+		check(mo->read_a == sizeof(mo->iv));
 	}
 
-	if(operation_mode != mk_operation_mode_ecb)
+	if(mo->operation_mode != mk_operation_mode_ecb)
 	{
-		mk_crypt_set_param_iv(&crypt, iv);
+		mk_crypt_set_param_iv(&mo->crypt, mo->iv);
 	}
 
-	buffer_a = buffer_1;
-	buffer_b = buffer_2;
-	read_a = fread(buffer_a, 1, sizeof(buffer_1) - mk_block_cipher_block_len_max, input_file);
-	if(read_a == 0)
+	mo->buffer_a = mo->buffer_1;
+	mo->buffer_b = mo->buffer_2;
+	mo->read_a = fread(mo->buffer_a, 1, sizeof(mo->buffer_1) - mk_block_cipher_block_len_max, mo->input_file);
+	if(mo->read_a == 0)
 	{
-		check(feof(input_file) != 0);
+		check(feof(mo->input_file) != 0);
 	}
-	if(read_a != 0)
+	if(mo->read_a != 0)
 	{
 		for(;;)
 		{
-			read_b = fread(buffer_b, 1, sizeof(buffer_1) - mk_block_cipher_block_len_max, input_file);
-			if(read_b == 0)
+			mo->read_b = fread(mo->buffer_b, 1, sizeof(mo->buffer_1) - mk_block_cipher_block_len_max, mo->input_file);
+			if(mo->read_b == 0)
 			{
-				check(feof(input_file) != 0);
+				check(feof(mo->input_file) != 0);
 				break;
 			}
 
-			if(direction == 0)
+			if(mo->direction == 0)
 			{
-				crypted = mk_crypt_encrypt(&crypt, 0, buffer_a, (int)read_a, buffer_a);
+				mo->crypted = mk_crypt_encrypt(&mo->crypt, 0, mo->buffer_a, (int)mo->read_a, mo->buffer_a);
 			}
 			else
 			{
-				crypted = mk_crypt_decrypt(&crypt, 0, buffer_a, (int)read_a, buffer_a);
+				mo->crypted = mk_crypt_decrypt(&mo->crypt, 0, mo->buffer_a, (int)mo->read_a, mo->buffer_a);
 			}
 
-			written = fwrite(buffer_a, 1, crypted, output_file);
-			check((int)written == crypted);
+			mo->written = fwrite(mo->buffer_a, 1, mo->crypted, mo->output_file);
+			check((int)mo->written == mo->crypted);
 
-			buffer_t = buffer_a;
-			buffer_a = buffer_b;
-			buffer_b = buffer_t;
-			read_t = read_a;
-			read_a = read_b;
-			read_b = read_t;
+			mo->buffer_t = mo->buffer_a;
+			mo->buffer_a = mo->buffer_b;
+			mo->buffer_b = mo->buffer_t;
+			mo->read_t = mo->read_a;
+			mo->read_a = mo->read_b;
+			mo->read_b = mo->read_t;
 		}
 	}
-	if(direction == 0)
+	if(mo->direction == 0)
 	{
-		crypted = mk_crypt_encrypt(&crypt, 1, buffer_a, (int)read_a, buffer_a);
+		mo->crypted = mk_crypt_encrypt(&mo->crypt, 1, mo->buffer_a, (int)mo->read_a, mo->buffer_a);
 	}
 	else
 	{
-		crypted = mk_crypt_decrypt(&crypt, 1, buffer_a, (int)read_a, buffer_a);
-		check(crypted != -1);
+		mo->crypted = mk_crypt_decrypt(&mo->crypt, 1, mo->buffer_a, (int)mo->read_a, mo->buffer_a);
+		check(mo->crypted != -1);
 	}
-	written = fwrite(buffer_a, 1, crypted, output_file);
-	check((int)written == crypted);
+	mo->written = fwrite(mo->buffer_a, 1, mo->crypted, mo->output_file);
+	check((int)mo->written == mo->crypted);
 
-	fclosed = fclose(output_file);
-	check(fclosed == 0);
+	mo->fclosed = fclose(mo->output_file);
+	check(mo->fclosed == 0);
 
-	fclosed = fclose(input_file);
-	check(fclosed == 0);
+	mo->fclosed = fclose(mo->input_file);
+	check(mo->fclosed == 0);
+
+	free(mo);
 
 	return 0;
 }
