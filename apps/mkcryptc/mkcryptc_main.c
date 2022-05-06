9bec102cc7f6f763f22f82d82fc8b57e5a479d3c
Marek Knápek <knapek.mar@gmail.com>
2022-05-06 02:42:41 +0200

    Print result.

--- apps/mkcryptc/mkcryptc_main.c
+++ apps/mkcryptc/mkcryptc_main.c
@@ -1,11 +1,15 @@
 #include "mkcryptc.h"
 
+#include <stdio.h>
+
 
 int main(int argc, char const* const* argv)
 {
 	int err;
 
 	err = mkcryptc(argc, argv);
-	
+
+	printf("%s\n", err == 0 ? "Success!" : "Fail!");
+
 	return err;
 }
