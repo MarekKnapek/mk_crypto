2de256b6ed180b024667a922bcb91a27d04874ef
Marek Knápek <knapek.mar@gmail.com>
2022-05-15 20:15:14 +0200

    Debug -> Release.

--- projects/jumbo/mk_win_hash_file/build_and_run.bat
+++ projects/jumbo/mk_win_hash_file/build_and_run.bat
@@ -1,4 +1,4 @@
 @echo off
 
-cl /nologo /W4 mk_win_hash_file.c /link /subsystem:windows user32.lib comdlg32.lib || exit /b
+cl /nologo /W4 /WX /O2 /Ob2 /D NDEBUG /D _CRT_SECURE_NO_WARNINGS mk_win_hash_file.c /link /subsystem:windows user32.lib comdlg32.lib || exit /b
 mk_win_hash_file.exe mk_win_hash_file.c
