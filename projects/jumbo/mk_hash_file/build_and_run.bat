d2f3a3f3c01190428952b10a026ff70beabda1a8
Marek Knápek <knapek.mar@gmail.com>
2022-05-15 20:08:26 +0200

    Get rid of mk_hash_file.

--- projects/jumbo/mk_hash_file/build_and_run.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-
-cl /nologo /W4 mk_hash_file.c || exit /b
-mk_hash_file.exe mk_hash_file.c
--- projects/jumbo/mk_hash_file/build_and_run.sh
+++
@@ -1,4 +0,0 @@
-set -e
-
-gcc -std=c89 -Wall -Wextra -Wpedantic -o mk_hash_file.exe mk_hash_file.c
-./mk_hash_file.exe mk_hash_file.c
