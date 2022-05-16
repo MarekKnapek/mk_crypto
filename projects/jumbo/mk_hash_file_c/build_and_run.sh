4eb9b4013ceec65a2b74366dc32130bd345812db
Marek Knápek <knapek.mar@gmail.com>
2022-05-16 19:00:48 +0200

    Tweak jumbo builds.

--- projects/jumbo/mk_hash_file_c/build_and_run.sh
+++ projects/jumbo/mk_hash_file_c/build_and_run.sh
@@ -1,4 +1,4 @@
 set -e
 
-gcc -std=c89 -O2 -DNDEBUG -Wall -Wextra -Wpedantic -o mk_hash_file_c.exe mk_hash_file_c.c
+gcc -std=c89 -O3 -DNDEBUG -Dmk_want_jumbo=1 -Wall -Wextra -Wpedantic -Wno-unused-function -o mk_hash_file_c.exe mk_hash_file_c.c
 ./mk_hash_file_c.exe mk_hash_file_c.c
--- projects/jumbo/mk_win_hash_file/build_and_run.sh
+++ projects/jumbo/mk_win_hash_file/build_and_run.sh
@@ -1,4 +1,4 @@
 set -e
 
-gcc -std=c89 -O3 -Wall -Wextra -Wpedantic -o mk_win_hash_file.exe mk_win_hash_file.c -lcomdlg32
+gcc -std=c89 -O3 -DNDEBUG -Dmk_want_jumbo=1 -Wall -Wextra -Wpedantic -Wno-unused-function -o mk_win_hash_file.exe mk_win_hash_file.c -lcomdlg32
 ./mk_win_hash_file.exe mk_win_hash_file.c
