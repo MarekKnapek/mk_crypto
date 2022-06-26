d06a030184cbb378ded48c619729c4d7915ce574
Marek Knápek <knapek.mar@gmail.com>
2022-06-26 08:44:54 +0200

    Use __debugbreak in MSVC debug mode.

--- src/utils/mk_try.c
+++ src/utils/mk_try.c
@@ -3,7 +3,23 @@
 #include "mk_jumbo.h"
 
 
+#if defined(NDEBUG)
+#else
+#if defined(_MSC_VER)
+#pragma warning(push)
+#pragma warning(disable:4019) /* warning C4019: empty statement at global scope */
+#include <intrin.h>
+#pragma warning(pop)
+#endif
+#endif
+
+
 mk_jumbo void mk_try_break_point(void)
 {
-	/* put break point or crash here */
+#if defined(NDEBUG)
+#else
+#if defined(_MSC_VER)
+	__debugbreak();
+#endif
+#endif
 }
