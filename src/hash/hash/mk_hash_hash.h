fbf6b650c8fa60fca976473f42d54338130ddefd
Marek Knápek <knapek.mar@gmail.com>
2022-07-02 22:17:41 +0200

    Hash include.

---
+++ src/hash/hash/mk_hash_hash.h
@@ -0,0 +1,32 @@
+#include "../base/mk_hash_base_alg_id.h"
+
+
+#if alg_id == alg_id_hash_md2
+#include "mk_hash_hash_md2.h"
+#elif alg_id == alg_id_hash_md4
+#include "mk_hash_hash_md4.h"
+#elif alg_id == alg_id_hash_md5
+#include "mk_hash_hash_md5.h"
+#elif alg_id == alg_id_hash_sha1
+#include "mk_hash_hash_sha1.h"
+#elif alg_id == alg_id_hash_sha2_224
+#include "mk_hash_hash_sha2_224.h"
+#elif alg_id == alg_id_hash_sha2_256
+#include "mk_hash_hash_sha2_256.h"
+#elif alg_id == alg_id_hash_sha2_384
+#include "mk_hash_hash_sha2_384.h"
+#elif alg_id == alg_id_hash_sha2_512
+#include "mk_hash_hash_sha2_512.h"
+#elif alg_id == alg_id_hash_sha2_512224
+#include "mk_hash_hash_sha2_512224.h"
+#elif alg_id == alg_id_hash_sha2_512256
+#include "mk_hash_hash_sha2_512256.h"
+#elif alg_id == alg_id_hash_sha3_224
+#include "mk_hash_hash_sha3_224.h"
+#elif alg_id == alg_id_hash_sha3_256
+#include "mk_hash_hash_sha3_256.h"
+#elif alg_id == alg_id_hash_sha3_384
+#include "mk_hash_hash_sha3_384.h"
+#elif alg_id == alg_id_hash_sha3_512
+#include "mk_hash_hash_sha3_512.h"
+#endif
