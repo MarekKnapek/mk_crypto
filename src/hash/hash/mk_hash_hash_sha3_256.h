47946864ba1d452a73c87cfb06048709cab3b849
Marek Knápek <knapek.mar@gmail.com>
2022-07-02 22:12:05 +0200

    Better template macros.

--- src/hash/hash/mk_hash_hash_sha3_256.h
+++ src/hash/hash/mk_hash_hash_sha3_256.h
@@ -4,9 +4,17 @@
 
 #include "../base/mk_hash_base_alg_id.h"
 
+#ifdef alg_id
+#define was_hash_hash
+#else
 #define alg_id alg_id_hash_sha3_256
+#endif
 #include "mk_hash_hash.h.inl"
+#ifdef was_hash_hash
+#undef was_hash_hash
+#else
 #undef alg_id
+#endif
 
 
 #endif
